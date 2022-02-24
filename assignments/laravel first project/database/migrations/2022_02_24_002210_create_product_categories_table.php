2020fb7baef3d0b372e1cea438da4faeccbf0c63
AishwaryaItworks <97504117+AishwaryaItworks@users.noreply.github.com>
2022-02-24 19:10:10 +0530

    Add files via upload

---
+++ assignments/laravel first project/database/migrations/2022_02_24_002210_create_product_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->bigIncrements('product_category_id');
+            $table->string('product_category_name',50);
+            $table->longText('product_category_description')->nullable();
+            $table->enum('product_category_status',array('Active','Inactive'))->Default('Active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};
